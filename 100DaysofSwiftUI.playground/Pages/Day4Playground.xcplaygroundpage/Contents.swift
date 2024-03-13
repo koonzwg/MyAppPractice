f4519d648e1eb1cdf8d7d6604c89473eff899fb0
koonzwg <koonzwg@gmail.com>
2024-03-13 14:24:41 -0500

    Day 4 complete

--- 100DaysofSwiftUI.playground/Pages/Day4Playground.xcplaygroundpage/Contents.swift
+++ 100DaysofSwiftUI.playground/Pages/Day4Playground.xcplaygroundpage/Contents.swift
@@ -2,6 +2,36 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+//Day 4 - Type Annotations and checkpoint 2
+//Type annotations
+let surname = "Lasso"
+var score = 0
+//Both of these use type inference: swift infers the data type because it is what we assigned to it
 
-//: [Next](@next)
+let surname1: String = "William" // Type annotations let us be explicit about what data types we want
+var score1: Double = 0
+
+
+/*Checkpoint 2
+1. create an array of strings
+2. write some code that prints the number of items in the array
+3. write some code that prints the number of unique items in the array */
+
+var names = [String]()
+
+names.append("William")
+names.append("John")
+names.append("Jeff")
+names.append("Alfred")
+names.append("Austin")
+names.append("William")
+names.append("Jeffrey")
+names.append("Janice")
+
+print(names.count)
+
+let uniqueNames = Set(names)
+
+print(uniqueNames.count)
+
+
